ce6d5425f7a9f2b691bd9493906dd961f50d2fd1
trevor oakley <sales@merrows.co.uk>
2020-04-10 19:56:03 +0000

    mongo

--- peerRestApi.js
+++ peerRestApi.js
@@ -8,6 +8,7 @@ const app = express();
 const pg = require('pg');
 
 require('dotenv').config();
+const mongoose = require('mongoose');
 
 request = require('request');
 const bodyParser = require('body-parser');
@@ -15,6 +16,19 @@ var wget = require('node-wget');
 var url = require('url');
 var path = require('path');
 
+mongoose.connect('mongodb://127.0.0.1:27017/mydb');
+
+
+var groupSchema = new mongoose.Schema({
+  AdminName : { type: String, default: null },
+  GroupName : { type: String, default: null},
+  Longitude : {type: Number, default: null},
+  Latitude : {type: Number, default: null},
+  GroupChannel : {type: String, default: null},
+  GroupDesc : {type: String, default: null},
+});
+
+var groupDB = mongoose.model("peergroup", groupSchema);
 
 app.get("/api/ping", function(req, res) {
     res.json({ message: "pong" });
@@ -32,7 +46,7 @@ app.use('/', express.static(path.join(__dirname, '/html')));
     next();
 });*/
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "ec2-54-174-166-252.compute-1.amazonaws.com"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
@@ -44,16 +58,34 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.post("/api/addgroup", function(req, res) {
     var adminname = req.body.adminname;
     var groupname = req.body.groupname;
-    var grouploc = req.body.grouploc;
+    var longitude = req.body.longitude;
+    var latitude = req.body.latitude;
     var groupchannel = req.body.groupchannel;
     var groupdesc = req.body.groupdesc;
     // var testBuffer = req.body.image;
-    console.log(adminname);
-    res.json({message: "Correct"})
+    var groupCreate = new groupDB({
+    AdminName : adminname,
+    GroupName : groupname,
+    Longitude : longitude,
+    Latitude : latitude,
+    GroupChannel : groupchannel,
+    GroupDesc : groupdesc
+    });
+
+  groupDB.findOne({ GroupName : groupname}, function(err, doc){
+    if(doc == null){
+      groupCreate.save(function(err, doc){
+        if(err) throw err;
+
+         res.json({ message: "Correct" });
+      });
+    }else{
+         res.json({ message: "group exists"});
+    }
+  });
+
 
 });
-
-
 
 
 // Starting both http & https servers
--- peerjoin.js
+++ peerjoin.js
@@ -1,20 +1,22 @@
 function addgroup() {
     adminname  = document.getElementById("adminname").value;
     groupname  = document.getElementById("groupname").value;
-    grouploc  = document.getElementById("grouploc").value;
+    longitude  = document.getElementById("longitude").value;
+    latitude  = document.getElementById("latitude").value;
     groupchannel  = document.getElementById("groupchannel").value;
     groupdesc  = document.getElementById("groupdesc").value;
     var xhttp = new XMLHttpRequest();
     xhttp.open("POST",  "http://52.204.175.119:3000/api/addgroup", true);
     xhttp.setRequestHeader("Content-Type", "application/json; charset=utf-8");
-    xhttp.send(JSON.stringify({ "adminname": adminname, "groupname": groupname, "grouploc": grouploc, "groupchannel": groupchannel, "groupdesc": groupdesc }));
+    xhttp.send(JSON.stringify({ "adminname": adminname, "groupname": groupname, "longitude": longitude, "latitude" : latitude, "groupchannel": groupchannel, "groupdesc": groupdesc }));
     xhttp.onreadystatechange = function(){
       if (this.readyState == 4 && this.status == 200) {
         var data = JSON.parse(this.response);
-        filename = data.cid[0].path;
-        fname = "https://ipfs.io/ipfs/"+ filename;
-        document.getElementById("addedfile").innerHTML=fname;
-        document.getElementById("addedfile").href=fname;
+        if (data.message === "Correct") {
+	      document.getElementById("message").innerHTML="Your group was added";
+	} else {
+              document.getElementById("message").innerHTML="There was an error";
+	}
       }
     };
 }
